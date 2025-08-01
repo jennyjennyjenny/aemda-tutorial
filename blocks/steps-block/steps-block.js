c18ecf241748e2be46696fe7ad2e994f23b65ae7
Jenny ham <jenny.ham@accenture.com>
2025-08-01 07:43:16 -0700

    typo

--- blocks/steps-block/steps-block.js
+++ blocks/steps-block/steps-block.js
@@ -24,7 +24,7 @@ const createMobileContent = (stepsContent) => {
 
   const mobileContents = stepsContent.map((item) => createMobileStep(item));
 
-  mobileContentContainer.append(JSON.parse(mobileContent));
+  mobileContentContainer.append(JSON.parse(mobileContents));
 
   return mobileContentContainer;
 };
